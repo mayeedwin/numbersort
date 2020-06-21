eea9bfac7c3ab5f2f052a3d47fd858915af7de94
Maye Edwin <mayedwine1@gmail.com>
2020-06-21 10:26:34 +0300

    Add Comments

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-// Sort Numbers...
+// Acsend...
 exports.Ascend = (a, b) => {
   return a - b;
 };
-
+// Descend...
 exports.Descend = (a, b) => {
   return b - a;
 };
-
+// Largest number...
 exports.largestNumber = arr => {
   return Math.max.apply(null, arr);
 };
-
+// Smallest number...
 exports.smallestNumber = arr => {
   return Math.min.apply(null, arr);
 };
